262cdbe1f45f0c934484562ee6c4b16e2161ef0c
Neil T. Dantam <ntd@rice.edu>
2016-08-02 16:49:02 -0500

    Add git backup function

--- src/git.lisp
+++ src/git.lisp
@@ -9,3 +9,36 @@
                  :function (lambda (pathname)
                              (run-command (list "git" "-C" (namestring pathname) "fsck"
                                                 "--full" "--no-dangling")))))
+(defun git-url-path (url)
+  (ppcre:register-groups-bind (host path)
+      (`(:sequence :start-anchor
+                   ;; strip https / username@
+                   (:greedy-repetition 0 1 (:regex "https?://"))
+                   (:greedy-repetition 0 1 (:regex ".*@"))
+                   ;; get host
+                   (:regex "([^/:]*)")
+                   (:alternation "/" ":")
+                   ;; path
+                   (:regex "(.*)"))
+        url)
+    (pathname (concatenate 'string
+                           host "/"
+                           (ppcre:regex-replace "\\.git$" path "")
+                           ".git"))))
+
+
+(defun %backup-git (root url)
+  (let* ((root (pathname root))
+         (rel-path (git-url-path url))
+         (abs-path (merge-pathnames rel-path root))
+         (namestring (namestring abs-path)))
+    (assert (null (pathname-name root)))
+    (if (probe-file abs-path)
+        ;; exists, update
+        (run-command (list  "git" "-C" namestring "fetch" "--prune"))
+        ;; does not exist clone
+        (run-command (list  "git" "clone" "--mirror" url namestring )))))
+
+(defun backup-git (root urls)
+  (loop for url in (ensure-list urls)
+     do (%backup-git root url)))
